b68619d79f0104c0d5878c2c3cdafab39cd56a24
Brandon Davis <brandon.davis@kitware.com>
2020-10-18 17:32:57 -0400

    remove artifacts

--- workspacesio/artifacts/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Artifacts are data objects deterministically generated from data managed in workspaces.
-These could be thumbnails, transcoded media, etc.
-
-The bytes live within the workspace, but they're costly enough to compute that it's worth
-tracking them within the workspaces database.
-"""
--- workspacesio/artifacts/crud.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from sqlalchemy import and_, func
-from sqlalchemy.orm import Session
-
-from workspacesio.common import schemas
-
-from . import models as artifact_models
